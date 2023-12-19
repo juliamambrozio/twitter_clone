75db32b17b163347c43581588057ea797cabe025
Julia Marques <122115178+juliamambrozio@users.noreply.github.com>
2023-12-19 14:37:28 -0300

    arquivos necessários

---
+++ App/Route.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use MF\Init\Bootstrap;
+
+class Route extends Bootstrap {
+
+	protected function initRoutes() {
+
+		$routes['home'] = array(
+			'route' => '/',
+			'controller' => 'indexController',
+			'action' => 'index'
+		);
+
+		$routes['inscreverse'] = array(
+			'route' => '/inscreverse',
+			'controller' => 'indexController',
+			'action' => 'inscreverse'
+		);
+
+		$this->setRoutes($routes);
+	}
+
+}
+
+?>
